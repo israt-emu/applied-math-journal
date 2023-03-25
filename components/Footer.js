e61857ea92c8c613f0956122e0031bd3d2880c92
Tushar@8640 <imtantushar44@gmail.com>
2023-03-25 21:55:30 +0600

    global css

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer>
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
